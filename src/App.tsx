0ff2e6d1eec5840ee346bee45585a9cbea196830
tungnt2 <tungnt2@vnpay.vn>
2023-03-24 16:44:48 +0700

    test precomiit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+                        <a href="https://vitejs.devabdc" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
