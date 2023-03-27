4e6cb0eba5bb07fa4ba25c24be12efc6c2ebd0c4
Tung <hihahihahoho@gmail.com>
2023-03-27 22:08:53 +0700

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,42 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 import svgr from 'vite-plugin-svgr';
-
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		svgr({
-			svgrOptions: {
-				svgo: true,
-				svgoConfig: {
-					plugins: [
-						{
-							name: '@svgr/plugin-svgo',
-							params: {
-								plugins: [
-									'preset-default',
-									{
-										name: 'addAttributesToSVGElement',
-										params: {
-											attributes: ['path', 'currentColor'],
-										},
-									},
-								],
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+	const baseURL = process.env.VITE_BASE_URL || '.';
+
+	return defineConfig({
+		base: baseURL,
+		plugins: [
+			svgr({
+				svgrOptions: {
+					plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+					svgo: true,
+					svgoConfig: {
+						plugins: [
+							'prefixIds',
+							{
+								name: 'addClassesToSVGElement',
+								params: {
+									className: 'svg-class',
+								},
 							},
-						},
-					],
+						],
+					},
+					// replaceAttrValues: { '#fff': 'currentColor' }, // Replace the desired color with 'currentColor'
 				},
-				// replaceAttrValues: { '#fff': 'currentColor' }, // Replace the desired color with 'currentColor'
-			},
-		}),
-		react(),
-		eslint(),
-		vitePluginFaviconsInject('./public/favicon.svg'),
-	],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: ['./src/setupTests.ts'],
-	},
-});
+			}),
+			react(),
+			eslint(),
+			vitePluginFaviconsInject(`./public/favicon.svg`, {}),
+		],
+		test: {
+			globals: true,
+			environment: 'jsdom',
+			setupFiles: ['./src/setupTests.ts'],
+		},
+	});
+};
