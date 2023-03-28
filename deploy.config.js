9369e472733c0295f12a007db45862a8a7870119
tungnt2 <tungnt2@vnpay.vn>
2023-03-28 11:40:09 +0700

    update

---
+++ deploy.config.js
@@ -0,0 +1,44 @@
+import { promises as fs } from 'fs';
+import FtpDeploy from 'ftp-deploy';
+import { loadEnv } from 'vite';
+import ftpConfig from './ftp.config.js';
+
+process.env = { ...process.env, ...loadEnv('design', process.cwd()) };
+
+const htaccessContent = `<IfModule mod_rewrite.c>
+  RewriteEngine On
+  RewriteBase /${process.env.VITE_BASE_URL}/
+  RewriteCond %{REQUEST_FILENAME} !-f
+  RewriteCond %{REQUEST_FILENAME} !-d
+  RewriteCond %{REQUEST_FILENAME} !-l
+  RewriteRule . /${process.env.VITE_BASE_URL}/index.html [L]
+</IfModule>`;
+
+async function generateHtaccessAndDeploy () {
+	try {
+		await fs.writeFile('./dist/.htaccess', htaccessContent);
+		console.log('File written successfully');
+
+		const ftpDeploy = new FtpDeploy();
+		const config = {
+			...ftpConfig,
+			remoteRoot: process.env.VITE_FTP_DIR,
+			localRoot: './dist',
+			include: ['.*', '**/*'],
+			// delete ALL existing files at destination before uploading, if true
+			deleteRemote: true,
+			// Passive mode is forced (EPSV command is not sent)
+			forcePasv: true,
+			// use sftp or ftp
+			sftp: false,
+		};
+		console.log(process.env.VITE_FTP_DIR);
+
+		await ftpDeploy.deploy(config);
+		console.log(`Finish uploaded to: ${process.env.VITE_DOMAIN}${process.env.VITE_BASE_URL}`);
+	} catch (error) {
+		console.error('Error occurred:', error);
+	}
+}
+
+generateHtaccessAndDeploy();
--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<BrowserRouter basename={import.meta.env.BASE_URL}>
+		<BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>,
